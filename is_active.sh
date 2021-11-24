8004d4bfd595198707c3b9d0b3b6182991ac9f54
Matteo <93927786+mattvx@users.noreply.github.com>
2021-11-24 15:48:58 +0100

    Update is_active.sh
    fixed code

--- is_active.sh
+++ is_active.sh
@@ -2,28 +2,41 @@
 
 # Check if the service actually exists by running a command
 if ! command -v chronyd &>/dev/null; then
-        printf 'Command chrony not found!\n'
-        exit
+	printf 'Command chrony not found!\n'
+	exit
 
 else
-        # If it exists and is active...
-        if [ "$(systemctl is-active chrony)" == "active" ]; then
-                printf 'Service is up and running!\n'
+	# If it exists and is active...
+	if [ "$(systemctl is-active chrony)" == "active" ]; then
+		printf 'Service is up and running!\n'
 
-        # If it exists but is not active... Try to restart
-        elif [ "$(systemctl is-active chrony)" == "inactive" ]; then
-                printf 'Found stopped service! Trying to restart...\n'
+	# If it exists but is not active... Try to restart
+	elif [ "$(systemctl is-active chrony)" == "inactive" ]; then
+		printf 'Found stopped service! Trying to restart...\n'
 
-                # Checks if the command actually works
-                if ! sudo systemctl start --quiet chrony; then
-                        printf 'something went wrong while starting chrony\n'
-                        exit
-                else
-                        printf '...Service started!\n'
-                fi
+		# Checks if the command actually works
+		if ! sudo systemctl start --quiet chrony; then
+			printf 'something went wrong while starting chrony\n'
+			exit
+		else
+			printf '...Service started!\n'
+		fi
 
-        else
-                printf 'Something is wrong. Try running systemctl status chrony\n'
-                exit
-        fi
+		# Check if the service is enabled
+		if [ "$(systemctl is-enabled chrony)" == "disabled" ]; then
+			printf 'Service is not enabled!\n...enabling!\n'
+			# Check if enabling the service fails...
+			if ! sudo systemctl enable --quiet chrony; then
+				printf 'Something went wrong while enabling the service.'
+				exit
+			else
+				printf 'Service enabled!'
+			fi
+		else
+			printf 'Service enabled!'
+		fi
+	else
+		printf 'Something is wrong. Try running systemctl status chrony\n'
+		exit
+	fi
 fi
